90e13a135d6b2f3f1842514e6fe3c6c7ffa93fa5
Lorri Lanig <lorrilanig.art@gmail.com>
2025-01-18 11:37:13 -0600

    initial commit

---
+++ artmission-api/src/main/java/com/lorrilanig/artmission_api/ArtmissionApiApplication.java
@@ -0,0 +1,13 @@
+package com.lorrilanig.artmission_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtmissionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtmissionApiApplication.class, args);
+	}
+
+}
