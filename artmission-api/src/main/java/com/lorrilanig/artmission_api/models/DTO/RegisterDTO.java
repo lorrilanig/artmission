70bfb130dbc81cf9421bf07ecbe5ae85e1628d6f
Lorri Lanig <lorrilanig.art@gmail.com>
2025-01-22 14:32:29 -0600

    registerDTO

--- artmission-api/src/main/java/com/lorrilanig/artmission_api/models/DTO/RegisterDTO.java
+++ artmission-api/src/main/java/com/lorrilanig/artmission_api/models/DTO/RegisterDTO.java
@@ -1,4 +1,55 @@
 package com.lorrilanig.artmission_api.models.DTO;
 
+import java.util.regex.Pattern;
+
 public class RegisterDTO {
+    private String username;
+    private String email;
+    private String password;
+    private String verifyPassword;
+
+    public static boolean isValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern patttern = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return patttern.matcher(email).matches();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
 }
