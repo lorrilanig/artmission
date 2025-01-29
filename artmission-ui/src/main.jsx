24d9b16786ed85eadd92699118a2d7028428272f
Lorri Lanig <lorrilanig.art@gmail.com>
2025-01-29 12:25:15 -0600

    router included, files and folders

--- artmission-ui/src/main.jsx
+++ artmission-ui/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//browser routes
+const router = createBrowserRouter([
+
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <App />
   </StrictMode>,
 )
