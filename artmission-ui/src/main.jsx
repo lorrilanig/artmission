46308414c34e1ee671273c092df290c395285797
Lorri Lanig <lorrilanig.art@gmail.com>
2025-01-30 11:01:32 -0600

    front end visual test working

--- artmission-ui/src/main.jsx
+++ artmission-ui/src/main.jsx
@@ -1,19 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 
 
 //browser routes
-const router = createBrowserRouter([
+// const router = createBrowserRouter([
 
-])
+// ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
     <App />
   </StrictMode>,
 )
